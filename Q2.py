da9df0903e865b85a56fbe05facbf1615b501417
dropTableUsers42 <rwitaban0001@gmail.com>
2019-08-25 00:42:33 +0530

    Added outlab3: Q1.py and Q3.py

---
+++ Q2.py
@@ -0,0 +1,34 @@
+import re
+import sys
+
+file = open(sys.argv[1])
+
+freq = {}
+
+for each in file:
+    emails=re.findall("\\b(?:[a-zA-Z0-9]+[\._]{0,1})+[a-zA-Z0-9]+@(?:[a-zA-Z0-9]+\.{0,1})+[a-zA-Z]+\\b",each)
+    numbers=re.findall("\\b[1-9][0-9]{9}\\b",each)
+    for match in emails:
+        if match not in freq:
+            freq[match]=1
+        else:
+            freq[match]=freq[match]+1
+    for match in numbers:
+        if match not in freq:
+            freq[match]=1
+        else:
+            freq[match]=freq[match]+1
+
+print('my frequency: ',end='')
+if sys.argv[2] not in freq:
+    myfreq=0
+else:
+    myfreq=freq[sys.argv[2]]
+print(myfreq)
+cheat=False
+for i in freq:
+    if(freq[i]>myfreq):
+        print('Cheater alert!',i,freq[i])
+        cheat=True
+if not cheat:
+    print('It\'s all good yo!')
